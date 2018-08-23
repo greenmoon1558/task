a589392eefee64a506d7b056be5750ce0b6dfea5
JanGordienko <svitlana.zhydejkina@gmail.com>
2018-08-24 00:16:16 +0300

    add task

---
+++ googleJSON.js
@@ -0,0 +1,96 @@
+ var googleJSON = {
+   "type": "FeatureCollection",
+   "features": [{
+       "id": 0,
+       "type": "Feature",
+       "properties": {
+         "letter": "G",
+         "color": "blue",
+         "rank": "7",
+         "ascii": "71",
+         "Description": "the letter G"
+       },
+       "geometry": {
+         "type": "Point",
+         "coordinates": [123.61, -22.14]
+
+       }
+     },
+     {
+       "id": 1,
+       "type": "Feature",
+       "properties": {
+         "letter": "o",
+         "color": "red",
+         "rank": "15",
+         "ascii": "111",
+         "Description": "the first letter o"
+       },
+       "geometry": {
+
+         "type": "Point",
+         "coordinates": [128.84, -25.76]
+       }
+     },
+     {
+       "id": 2,
+       "type": "Feature",
+       "properties": {
+         "letter": "o",
+         "color": "yellow",
+         "rank": "15",
+         "ascii": "111",
+         "Description": "the second letter o"
+       },
+       "geometry": {
+         "type": "Point",
+         "coordinates": [131.87, -25.76]
+       }
+     },
+     {
+       "id": 3,
+       "type": "Feature",
+       "properties": {
+         "letter": "g",
+         "color": "blue",
+         "rank": "7",
+         "ascii": "103",
+         "Description": "the letter g"
+       },
+       "geometry": {
+         "type": "Point",
+         "coordinates": [138.12, -25.04]
+       }
+     },
+     {
+       "id": 4,
+       "type": "Feature",
+       "properties": {
+         "letter": "l",
+         "color": "green",
+         "rank": "12",
+         "ascii": "108",
+         "Description": "the letter l"
+       },
+       "geometry": {
+         "type": "Point",
+         "coordinates": [140.14, -21.04]
+       }
+     },
+     {
+       "id": 5,
+       "type": "Feature",
+       "properties": {
+         "letter": "e",
+         "color": "red",
+         "rank": "5",
+         "ascii": "101",
+         "Description": "the letter e"
+       },
+       "geometry": {
+         "type": "Point",
+         "coordinates": [144.14, -27.41]
+       }
+     }
+   ]
+ }
